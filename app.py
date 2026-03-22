0caf4853ade85e8d6e112571177074c92a89bafa
clvshygiene <clvs_hygiene@g.clvs.tyc.edu.tw>
2026-03-22 22:33:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ sys_env = st.secrets.get("ENV", "PROD")
 
 # [V5.31 Patch 1] 確保 set_page_config 是第一個執行的指令
 
-if sys_env == “DEV”:
+if sys_env == "DEV":
 st.set_page_config(page_title=“🔧測試版-中壢家商，衛愛而生”, layout=“wide”, page_icon=“🧹”)
 st.sidebar.info(f”🕵️‍♀️ 系統目前抓到的身分證是：[{sys_env}]”)
 st.warning(“🚧 **目前位於 DEV 測試環境！** 在這裡送出的資料僅供測試，不會影響正式成績。”)
